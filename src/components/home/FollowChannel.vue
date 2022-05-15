cc07878266ad50d5bce8dc0ee13b57f363bf39ea
tkc <chuongtangkhanh2104@gmail.com>
2022-05-15 07:07:35 +0700

    fix: adjust avatar in follow page

--- src/components/home/FollowChannel.vue
+++ src/components/home/FollowChannel.vue
@@ -4,7 +4,12 @@
     style="text-decoration: none"
   >
     <div class="d-flex flex-column align-items-center">
-      <v-img width="96px" height="96px" :src="channel.avatar"></v-img>
+      <v-img
+        width="96px"
+        height="96px"
+        :src="channel.avatar"
+        class="img-responsive rounded-circle"
+      ></v-img>
       <br />
       <h5 style="width: 70%; text-align: center">
         <b>{{ channel.username }}</b>
