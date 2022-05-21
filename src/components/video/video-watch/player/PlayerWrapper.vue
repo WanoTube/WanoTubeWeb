402e06af1ea2cd6a33ec7e25401c519a821a4786
tkc <chuongtangkhanh2104@gmail.com>
2022-05-21 23:01:52 +0700

    fix: video with HLS format + different quality

--- src/components/video/video-watch/player/PlayerWrapper.vue
+++ src/components/video/video-watch/player/PlayerWrapper.vue
@@ -4,26 +4,27 @@
     style="overflow-y: scroll"
   >
     <div class="player-wrapper">
-      <vue-plyr :options="playerOptions" ref="plyr">
-        <video
-          class="player"
-          controls
-          playsinline
-          :data-poster="video.thumbnail_url"
-        >
-          <source size="720" :src="video.url" type="video/mp4" />
-        </video>
-      </vue-plyr>
+      <video
+        class="player"
+        ref="videoPlayer"
+        :style="`opacity: ${playerOptions.quality ? 1 : 0}`"
+        controls
+        playsinline
+        :data-poster="video.thumbnail_url"
+      ></video>
     </div>
     <SuggesttedVideos :video="video" />
   </div>
 </template>
 <script>
+import Hls from "hls.js";
+import Plyr from "plyr";
 import SuggesttedVideos from "./SuggesttedVideos.vue";
 export default {
   props: ["video", "onPlay"],
   data() {
     return {
+      hls: null,
       playerOptions: {
         controls: [
           "play-large",
@@ -39,28 +40,68 @@ export default {
           "fullscreen",
         ],
         invertTime: false,
-        settings: ["quality", "speed"],
+        settings: ["quality", "speed", "captions"],
+        quality: null,
+        i18n: {
+          qualityLabel: {
+            0: "Auto",
+          },
+        },
       },
     };
   },
   components: {
     SuggesttedVideos,
   },
-  mounted: function () {
-    this.$refs.plyr.player.on("play", this.onPlay);
+  methods: {
+    updateQuality(newQuality) {
+      if (!this.hls) return;
+      if (newQuality === 0) {
+        this.hls.currentLevel = -1; //Enable AUTO quality if option.value = 0
+      } else {
+        this.hls.levels.forEach((level, levelIndex) => {
+          if (level.height === newQuality) {
+            console.log("Found quality match with " + newQuality);
+            this.hls.currentLevel = levelIndex;
+          }
+        });
+      }
+    },
   },
-  watch: {
-    video: function (val) {
-      const player = this.$refs.plyr.player;
-      player.source = {
-        type: "video",
-        sources: [
-          {
-            src: val.url,
-          },
-        ],
-      };
-      player.currentTime = 0;
+  mounted() {
+    const player = this.$refs.videoPlayer;
+    // const url = this.video.url;
+    const url =
+      "https://d1td7i7nd90xol.cloudfront.net/output/0.14/cambongfa/cambongfa.m3u8.m3u8";
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(url);
+      hls.on(Hls.Events.MANIFEST_PARSED, (e, data) => {
+        const availableQualities = data.levels.map((l) => l.height);
+        this.playerOptions.quality = {
+          default: 0, //Default - AUTO
+          options: availableQualities,
+          forced: true,
+          onChange: (e) => this.updateQuality(e),
+        };
+        const plyr = new Plyr(player, this.playerOptions);
+        hls.attachMedia(this.$refs.videoPlayer);
+        this.hls = hls;
+        plyr.on("play", this.onPlay);
+      });
+    } else {
+      player.src = url;
+      new Plyr(player, this.defaultOptions);
+    }
+  },
+  computed: {
+    isHlsFormat() {
+      if (!this.video.url) return true;
+      return (
+        this.video.url.split(".")[this.video.url.split(".").length - 1] ===
+        "m3u8"
+      );
     },
   },
 };
@@ -69,7 +110,7 @@ export default {
 <style>
 .player-wrapper {
   width: 100%;
-  background: red;
+  background: black;
 }
 .player {
   width: 100%;
